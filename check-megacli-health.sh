58bf05bcda739a7991e7246da148ba58e0036883
chrone <charlesalva@gmail.com>
2016-03-24 02:00:03 +0700

    Update check-megacli-health.sh
    add link speed.

--- check-megacli-health.sh
+++ check-megacli-health.sh
@@ -2,7 +2,7 @@
 
 # Check MegaRAID RAID health status with MegaCLI
 # Device ID and Slot numbers will be used later to check SMART attributes and RAID rebuild progress status
-megacli -PDlist -aALL -NoLog | egrep 'Slot|Device|state' | awk '/Device ID/{if (x)print x;x="";}{x=(!x)?$0:x" -"$0;}END{print x;}' | sed 's/Firmware state://g'
+megacli -PDlist -aALL -NoLog | egrep 'Slot|Device|Link Speed|state' | awk '/Device ID/{if (x)print x;x="";}{x=(!x)?$0:x" -"$0;}END{print x;}' | sed 's/Firmware state://g'
 
 # Check drive SMART Attributes
 # Replace $DeviceID with Device ID number and X with drive order
